9f8ed28b10f0b2e82328cd35164fe6810ce0d696
Matan Shaviro <matan3sh@gmail.com>
2020-08-03 13:20:28 +0300

    Finish Bonus Part

--- app/containers/TicketList/index.js
+++ app/containers/TicketList/index.js
@@ -1,35 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TicketItem from './TicketItem';
 
-const tickets = [
+const initialTickets = [
   {
     id: 1,
-    title: 'Need a Little Help with Your Site?',
+    title: 'Need a Little Help with Your Site?1',
     content: 'Here at Wix we support you to community forum',
     by: 'matan3sh@gmail.com',
-    createdAt: Date.now(),
+    createdAt: '11/13/2008, 2:13:55 PM',
+    labels: ['corvid'],
   },
   {
     id: 2,
-    title: 'Need a Little Help with Your Site?',
+    title: 'Need a Little Help with Your Site?2',
     content: 'Here at Wix we support you to community forum',
     by: 'matan3sh@gmail.com',
-    createdAt: Date.now(),
+    createdAt: '18/13/2008, 6:13:55 PM',
+    labels: ['corvid', 'Api'],
   },
   {
     id: 3,
-    title: 'Need a Little Help with Your Site?',
+    title: 'Need a Little Help with Your Site?3',
     content: 'Here at Wix we support you to community forum',
     by: 'matan3sh@gmail.com',
-    createdAt: Date.now(),
+    createdAt: '20/13/2008, 1:13:55 PM',
+    labels: ['api'],
   },
 ];
 
 const TicketList = () => {
+  const [tickets, setTickets] = useState(initialTickets);
+
+  const onHideTicket = ticket => {
+    const updatedTicketList = tickets.filter(
+      currentTicket => ticket.id !== currentTicket.id,
+    );
+    setTickets(updatedTicketList);
+  };
+
   return (
-    <div>
-      {tickets.map(ticket => (
-        <TicketItem ticket={ticket} id={ticket.id} />
+    <div className="ticket-list">
+      <h3>Showing {tickets.length} Results</h3>
+      {tickets.map((ticket, index) => (
+        <TicketItem ticket={ticket} key={index} onHideTicket={onHideTicket} />
       ))}
     </div>
   );
